f532ffad0c56784791a1a8fc74831ebb9639baef
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-25 15:11:03 +0500

    Update Triangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -1,7 +1,25 @@
 package school.mjc.stage0.loops.task5;
 
+
+import java.io.ByteArrayOutputStream;
+
 public class Triangle {
-    public void printTriangle(int cathetusLength) {
+
+    public  void printTriangle(int cathetusLength) {
+
+        for (int x = 1; x <= cathetusLength; x++)
+        {
+            for (int i = 1; i <= x; i++)
+            {
+                System.out.print("8");
+            }
+            System.out.println("");
+        }
+
 
     }
+
+
 }
+
+
